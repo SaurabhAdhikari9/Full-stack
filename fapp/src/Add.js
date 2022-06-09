828539009c4d601ba938e9dba692db4abd9c8aab
Saurabh Adhikari <83704793+SaurabhAdhikari9@users.noreply.github.com>
2022-06-09 12:55:30 +0545

    adding created

--- fapp/src/Add.js
+++ fapp/src/Add.js
@@ -1,8 +1,48 @@
 import React from 'react'
-
+import { Form, Input, Button, Modal} from 'antd';
 function Add() {
   return (
-    <div>Add</div>
+    <div>
+      <Form>
+
+        <Form.Item
+        label="Title:"
+        >
+          <Input
+          placeholder='Title'/>
+        </Form.Item>
+
+        <Form.Item
+        label="Description:"
+        >
+          <Input
+          placeholder='Description'/>
+        </Form.Item>
+
+        <Form.Item
+        label="Catagory:"
+        >
+          <Input
+          placeholder='Category'/>
+        </Form.Item>
+
+        <Form.Item
+        label="Author:"
+        >
+          <Input
+          placeholder='Author'/>
+        </Form.Item>
+
+        <Form.Item
+        label="Signees:"
+        >
+          <Input
+          placeholder='Signees'/>
+        </Form.Item>
+
+
+      </Form>
+    </div>
   )
 }
 
