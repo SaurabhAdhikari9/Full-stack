2e9a5c94882853679da8439909384062f14cd1ce
Saurabh Adhikari <83704793+SaurabhAdhikari9@users.noreply.github.com>
2022-06-10 10:24:12 +0545

    made interaction with api

--- fapp/src/Add.js
+++ fapp/src/Add.js
@@ -1,6 +1,26 @@
-import React from 'react'
-import { Form, Input, Button, Modal} from 'antd';
+import React, { useState } from 'react'
+import { Form, Input, Button} from 'antd';
+import http from './http';
 function Add() {
+  const [data, setData] = useState({
+    title:'',
+    description:'',
+    category:'',
+    author:'',
+    signees:''
+  });
+
+  const show  = () => {
+    console.log(data.title);
+    console.log(data.description);
+    console.log(data.author);
+    console.log(data.category);
+    console.log(data.signees);
+  };
+
+  const addData = async () =>{
+    const reponse = await http.post("/api", data);
+  };
   return (
     <div>
       <Form>
@@ -9,37 +29,48 @@ function Add() {
         label="Title:"
         >
           <Input
-          placeholder='Title'/>
+          placeholder='Title'
+          onChange={(e) => setData({...data, title:e.target.value})}
+          />
         </Form.Item>
 
         <Form.Item
         label="Description:"
         >
           <Input
-          placeholder='Description'/>
+          placeholder='Description'
+          onChange={(e) => setData({...data,description:e.target.value})}
+          />
         </Form.Item>
 
         <Form.Item
         label="Catagory:"
         >
           <Input
-          placeholder='Category'/>
+          placeholder='Category'
+          onChange={ (e) => setData({...data, category:e.target.value})}
+          /> 
         </Form.Item>
 
         <Form.Item
         label="Author:"
         >
           <Input
-          placeholder='Author'/>
+          placeholder='Author'
+          onChange={ (e) => setData({...data, author:e.target.value})}
+          />
         </Form.Item>
 
         <Form.Item
         label="Signees:"
         >
           <Input
-          placeholder='Signees'/>
+          placeholder='Signees'
+          onChange={ (e) => setData({...data, signees:e.target.value})}
+          />
         </Form.Item>
-
+        <Button onClick={() => {addData(); show();}}>Add</Button>
+        <Button>Cancel</Button>
 
       </Form>
     </div>
--- fapp/src/Delete.js
+++ fapp/src/Delete.js
@@ -1,8 +1,28 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { Form, Input, Button, Modal} from 'antd';
+import http from './http';
 function Delete() {
+  const [deleteID, setDeleteID] = useState();
+
+  const deleteData = async () =>{
+    await http.delete(`/api/${deleteID}`)
+  };
   return (
-    <div>Delete</div>
+    <div>
+
+      <Form>
+        <Form.Item
+        label="ID"
+        >
+          <Input
+          placeholder='ID'
+          onChange={(e) => setDeleteID(e.target.value)}          
+          />
+        </Form.Item>
+        <Button>Cancel</Button>
+        <Button onClick={deleteData()}>Delete</Button>
+      </Form>
+    </div>
   )
 }
 
